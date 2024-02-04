b73fa538f5b16725805df211ee31b1d40e222be4
daikimorimoto <nanasinodonbei>
2024-02-04 21:12:30 +0900

    テスト

---
+++ src/test2/test23.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class test23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "faga";
+		char d = a.charAt(3);
+		
+		System.out.println( "\n" + d);
+		
+
+	}
+
+}
